44a18ca0d988bbfb34aaa96a564b28ba67c01583
Scott Searcy <ssearcy@roguecu.org>
2018-07-09 14:56:54 -0700

    Make the command prefix configurable. Added a basic dice roll function.

--- discord_foosball/__main__.py
+++ discord_foosball/__main__.py
@@ -1,33 +1,54 @@
 """Discord bot to help facilitate foosball games."""
-import discord
 import asyncio
+import random
+
+import discord
 
 from .config import config
 
 client = discord.Client()
 
 
+async def roll_dice(message: discord.Message):
+    dice_roll = random.randint(1, 20)
+    roller = message.author.nick or message.author.name
+    response = f'{roller} rolled a {dice_roll}'
+    await client.send_message(message.channel, response)
+
+
+COMMAND_MAP = {
+    'roll': roll_dice,
+}
+
+
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(f'Logged in as {client.user.name} ({client.user.id})')
     print('------')
+    print(f'Command prefix is "{config.command_prefix}"')
 
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
+    """Process messages sent to the channel."""
+    if not message.content.startswith(config.command_prefix):
+        return
 
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+    command = message.content[1:].split()[0]
+
+    await COMMAND_MAP[command](message)
+
+    # if message.content.startswith('!test'):
+    #     counter = 0
+    #     tmp = await client.send_message(message.channel, 'Calculating messages...')
+    #     async for log in client.logs_from(message.channel, limit=100):
+    #         if log.author == message.author:
+    #             counter += 1
+    #
+    #     await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    # elif message.content.startswith('!sleep'):
+    #     await asyncio.sleep(5)
+    #     await client.send_message(message.channel, 'Done sleeping')
 
 
 client.run(config.token)
--- discord_foosball/config.py
+++ discord_foosball/config.py
@@ -6,7 +6,9 @@ __all__ = ['config']
 
 @environ.config(prefix='DISCORD')
 class AppConfig:
+    # this is overkill for this application but I wanted to test it out
     token = environ.var()
+    command_prefix = environ.var(default='!')
 
 
 load_dotenv()
